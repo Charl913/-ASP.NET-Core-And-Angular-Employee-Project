ea9e2cd216a1c1ee8b8460fc5b01f583a8c4a125
Charl913 <charlkruger61@gmail.com>
2023-07-30 11:07:40 +0200

    Creation Of Walking Skeleton Angular

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
