b474670cc48b97444814dc8c337a0ab902c3ba52
Charl913 <charlkruger61@gmail.com>
2023-10-14 12:16:01 +0200

    Create Data Transfering Objects & Base Controller

---
+++ API/DTOs/LoginDTO.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs
+{
+    public class LoginDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ API/DTOs/RegisterDTO.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDTO
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
