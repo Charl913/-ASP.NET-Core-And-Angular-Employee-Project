061e97360e4b9911dfa24e9362f31e866690fa5e
Charl913 <charlkruger61@gmail.com>
2024-03-09 11:35:08 +0200

    Update Edit Profile

--- client/src/app/edit-profile/edit-profile.component.ts
+++ client/src/app/edit-profile/edit-profile.component.ts
@@ -1,29 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Employee } from '../_models/employee';
 import { AccountService } from '../_services/account.service';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faSquarePlus } from '@fortawesome/free-solid-svg-icons';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.css']
 })
-export class EditProfileComponent implements OnInit{
+export class EditProfileComponent implements OnInit {
   faPenToSquare = faPenToSquare;
+  faSquarePlus = faSquarePlus;
   currentEmployee: Employee = {} as Employee
+  modalRef?: BsModalRef;
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService,
+    private modalService: BsModalService) {
 
   }
 
   ngOnInit(): void {
     this.accountService.currentEmployee$.subscribe({
       next: res => {
-        if(res){
+        if (res) {
           this.currentEmployee = res;
         }
       },
       error: error => console.log(error)
     });
+  }
+
+  openModal(template: TemplateRef<void>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  saveEducation() {
+    this.modalService.hide();
   }
 }
